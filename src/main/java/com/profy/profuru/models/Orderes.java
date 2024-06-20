08e45f928e0e5681baf6fa3de12789d0f85adf13
VladislavHarcev <154264617+VladislavHarcev@users.noreply.github.com>
2024-06-20 10:45:21 +0300

    Add files via upload

---
+++ src/main/java/com/profy/profuru/models/Orderes.java
@@ -0,0 +1,101 @@
+package com.profy.profuru.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name="orderes")
+public class Orderes {
+
+    @Id
+    private UUID id;
+
+    @Column(name="executor_id")
+    private UUID executor_id;
+
+    @Column(name="customer_id")
+    private UUID customer_id;
+
+    @Column(name="state_id")
+    private UUID state_id;
+
+    @Column(name="date")
+    private Long date;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="descript")
+    private String descript;
+
+    public Orderes(UUID id, UUID executor_id, UUID customer_id, UUID state_id, Long date, String title, String descript){
+        this.id =id;
+        this.executor_id = executor_id;
+        this.customer_id = customer_id;
+        this.state_id = state_id;
+        this.date = date;
+        this.title = title;
+        this.descript = descript;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getExecutor_id() {
+        return executor_id;
+    }
+
+    public void setExecutor_id(UUID executor_id) {
+        this.executor_id = executor_id;
+    }
+
+    public UUID getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(UUID customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public UUID getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(UUID state_id) {
+        this.state_id = state_id;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+}
--- src/main/resources/db/migration/V1__firstmig.sql
+++ src/main/resources/db/migration/V1__firstmig.sql
@@ -1,3 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS migrations;
 CREATE TABLE public."order" (
 	executor_id uuid NULL,
 	id uuid DEFAULT gen_random_uuid() NOT NULL,
@@ -10,24 +11,3 @@ CREATE TABLE public."order" (
 	CONSTRAINT order_status_fk FOREIGN KEY (state_id) REFERENCES public.status(id)
 );
 CREATE INDEX order_id_idx ON public."order" USING btree (id, date, title, descript, state_id, executor_id, customer_id);
-
-CREATE TABLE public.status (
-	id uuid DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar NOT NULL,
-	CONSTRAINT state_unique_1 UNIQUE (name),
-	CONSTRAINT status_pk PRIMARY KEY (id)
-);
-
-CREATE TABLE public.customer (
-	"date" int8 NULL,
-	id uuid NOT NULL,
-	name varchar(255) NULL,
-	CONSTRAINT customer_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.executor (
-	"date" int8 NULL,
-	id uuid NOT NULL,
-	"name" varchar(255) NULL,
-	CONSTRAINT executor_pkey PRIMARY KEY (id)
-);
